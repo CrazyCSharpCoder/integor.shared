df471a453f19fc584cc4e8ac9b6279e0ede83b9f
Stephen L <logunov.stepan2001@gmail.com>
2023-03-15 01:43:24 +0700

    Added web-api middleware

---
+++ IntegorAspHelpers/Middleware/WebApiResponse/WebApiResponseProcessingAppBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Builder;
+
+namespace IntegorAspHelpers.Middleware.WebApiResponse
+{
+    using Internal;
+
+    public static class WebApiResponseProcessingAppBuilderExtensions
+    {
+        public static void UseWebApiExceptionsHandling(this IApplicationBuilder app, WriteBodyDelegate bodyWriter)
+        {
+            app.UseMiddleware<WebApiExceptionsHandlingMiddleware>(bodyWriter);
+        }
+
+        public static void UseWebApiStatusCodesHandling(this IApplicationBuilder app, WriteBodyDelegate bodyWriter, ValidateHttpContextActionDelegate? checkProcessingRequired = null)
+        {
+            app.UseMiddleware<WebApiStatusCodesHandlingMiddleware>(bodyWriter, checkProcessingRequired);
+        }
+    }
+}
