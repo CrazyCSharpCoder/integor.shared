d6e54db5ce06351f8949f9345b45b5c38b5e21b8
Stephen L <logunov.stepan2001@gmail.com>
2023-03-15 02:00:43 +0700

    Set actual assembly version of IntegorGlobalConstants

--- IntegorGlobalConstants/AssemblyInfo.cs
+++ IntegorGlobalConstants/AssemblyInfo.cs
@@ -2,8 +2,8 @@
 
 [assembly: AssemblyCompanyAttribute("IntegorGlobalConstants")]
 [assembly: AssemblyConfigurationAttribute("Release")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: AssemblyProductAttribute("IntegorGlobalConstants")]
 [assembly: AssemblyTitleAttribute("IntegorGlobalConstants")]
-[assembly: AssemblyVersionAttribute("1.0.0")]
+[assembly: AssemblyVersionAttribute("1.0.1")]
