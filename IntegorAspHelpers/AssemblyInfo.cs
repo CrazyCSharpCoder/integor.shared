7004b4db4c9de60a81956d97d43d28aa639a841c
Stephen L <logunov.stepan2001@gmail.com>
2023-03-15 01:07:00 +0700

    Added assembly infos

---
+++ IntegorAspHelpers/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompanyAttribute("IntegorAspHelpers")]
+[assembly: AssemblyConfigurationAttribute("Release")]
+[assembly: AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.0.1")]
+[assembly: AssemblyProductAttribute("IntegorAspHelpers")]
+[assembly: AssemblyTitleAttribute("IntegorAspHelpers")]
+[assembly: AssemblyVersionAttribute("1.0.0")]
