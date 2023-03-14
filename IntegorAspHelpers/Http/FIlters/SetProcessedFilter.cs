c1738b27fc79e32c819986294bde47e22d14ef9f
Stephen L <logunov.stepan2001@gmail.com>
2023-03-15 01:13:58 +0700

    Added SetProcessedFilter

---
+++ IntegorAspHelpers/Http/FIlters/SetProcessedFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace IntegorAspHelpers.Http.Filters
+{
+	public class SetProcessedFilter : IActionFilter
+	{
+		private IHttpContextProcessedMarker _processedMarker;
+
+		public SetProcessedFilter(IHttpContextProcessedMarker processedMarker)
+		{
+			_processedMarker = processedMarker;
+		}
+
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			_processedMarker.SetProcessed(true);
+		}
+	}
+}
