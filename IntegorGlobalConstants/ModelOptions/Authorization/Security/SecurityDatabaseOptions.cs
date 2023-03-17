569401d45b2ee846a15cbc7e5232f3b6183639c3
Stephen L <logunov.stepan2001@gmail.com>
2023-03-18 01:51:59 +0700

    Add model options

---
+++ IntegorGlobalConstants/ModelOptions/Authorization/Security/SecurityDatabaseOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegorGlobalConstants.ModelOptions.Authorization.Security
+{
+	public static class SecurityDatabaseOptions
+	{
+		public const int PasswordSaltLength = 64 / 8;
+	}
+}
