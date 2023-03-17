dac94e55d97b8f05430836cef817e297c7768575
Stephen L <logunov.stepan2001@gmail.com>
2023-03-18 02:37:15 +0700

    Added public dtos

---
+++ IntegorPublicDto/Authorization/Users/Input/LoginUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+using static IntegorGlobalConstants.ModelOptions.Authorization.UserAccountOptions;
+using static IntegorGlobalConstants.ExtraDtoOptions.Authorization.AuthorizationDtoOptions;
+
+using static IntegorGlobalConstants.DtoValidationMessages.DataAnnotationsErrors;
+using static IntegorGlobalConstants.DtoValidationMessages.Authorization.AuthorizationErrors;
+
+namespace IntegorPublicDto.Authorization.Users.Input
+{
+    public class LoginUserDto
+    {
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [EmailAddress(ErrorMessage = EmailAddressErrorMessage)]
+        [MaxLength(EMailLength, ErrorMessage = MaxStringLengthErrorMessage)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        [RegularExpression(PasswordValidatonRegex, ErrorMessage = IncorrectPasswordErrorMessage)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ IntegorPublicDto/Authorization/Users/UserAccountInfoDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegorPublicDto.Authorization.Users
+{
+	using Roles;
+
+	public class UserAccountInfoDto
+	{
+		public int Id { get; set; }
+
+		public string EMail { get; set; } = null!;
+
+		public DateTime PasswordUpdatedDate { get; set; }
+
+		public bool IsActive { get; set; }
+		public DateTime? IsActiveUpdatedDate { get; set; }
+
+		public DateTime CreatedDate { get; set; }
+
+		public UserRoleShortDto Role { get; set; } = null!;
+		public DateTime RoleUpdatedDate { get; set; }
+	}
+}
