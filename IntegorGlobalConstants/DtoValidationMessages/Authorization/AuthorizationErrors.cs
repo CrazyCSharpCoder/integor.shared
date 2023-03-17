2b175cf30299d4964ddc4fc9633adbeb1fd8146f
Stephen L <logunov.stepan2001@gmail.com>
2023-03-18 02:08:00 +0700

    Add dto options and messages

---
+++ IntegorGlobalConstants/DtoValidationMessages/Authorization/AuthorizationErrors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegorGlobalConstants.DtoValidationMessages.Authorization
+{
+	public static class AuthorizationErrors
+	{
+		public const string IncorrectPasswordErrorMessage =
+			"The \"{0}\" field can contain only the Latin alphabet characters and digits. " +
+			"Additionally it must have at least one digit, one lowercase and one uppercase Latin characters";
+	}
+}
---
+++ IntegorGlobalConstants/ExtraDtoOptions/Authorization/AuthorizationDtoOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegorGlobalConstants.ExtraDtoOptions.Authorization
+{
+	public static class AuthorizationDtoOptions
+	{
+		public const int MinPasswordLength = 8;
+		public const string PasswordValidatonRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]+$";
+	}
+}
