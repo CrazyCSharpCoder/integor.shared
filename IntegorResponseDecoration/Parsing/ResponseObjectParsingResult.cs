c95406e92de1aac867be7119916014d1b226326a
Stephen L <logunov.stepan2001@gmail.com>
2023-03-22 15:40:52 +0700

    Generic parsers and parsing result

---
+++ IntegorResponseDecoration/Parsing/ResponseObjectParsingResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegorResponseDecoration.Parsing
+{
+	public class ResponseObjectParsingResult<TValue>
+	{
+		public bool Success { get; }
+		public TValue? Value { get; }
+
+		public ResponseObjectParsingResult(bool success)
+		{
+			Success = success;
+		}
+
+		/// <summary>
+		/// Successful result with a new body for response
+		/// </summary>
+		public ResponseObjectParsingResult(TValue? newValue)
+		{
+			Success = true;
+			Value = newValue;
+		}
+	}
+}
