9a83b4736c6223c6be3c2e6b456211c05ec115bf
Stephen L <logunov.stepan2001@gmail.com>
2023-03-31 19:32:25 +0700

    Add HttpConstants

---
+++ IntegorGlobalConstants/HttpConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegorGlobalConstants
+{
+	public static class HttpConstants
+	{
+		public const string AccessTokenCookieName = "AuthenticationAccess";
+		public const string RefreshTokenCookieName = "AuthenticationRefresh";
+	}
+}
